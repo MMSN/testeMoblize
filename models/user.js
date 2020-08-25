ffd5a87066db4f33a689042a4a82f66a838ec79a
Mateus Moreira Silveira do Nascimento <mateus@Mateuss-MacBook-Pro.local>
2020-08-24 21:15:21 -0300

    Feito o sistema para adicionar usuarios. Ainda vai ser necessario certos ajustos conforme for adicionado os outros models.

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    telefone: {
+        type: String,
+        required: true,
+    },
+    birthdate: {
+        type: Date,
+        required: true,
+    },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
